1fab6064711bd86972130d4265397b7f99c69c86
Jasper5143 <jamieson.jasper@lindisfarne.school.nz>
2024-07-29 12:42:30 +1200

    Sprint 1

---
+++ Scripts/Death screen.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Prefabs/game.tscn")
+
+
+
+func _on_go_to_menu_pressed():
+	get_tree().change_scene_to_file("res://Prefabs/menu.tscn")
